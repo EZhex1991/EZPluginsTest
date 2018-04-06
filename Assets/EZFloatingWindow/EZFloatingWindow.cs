bcd8c7dcbdbfe560e53d9d7a7cfb5645a057495c
Zack John <ezhex1991@outlook.com>
2018-04-06 12:15:27 +0000

    first commit

---
+++ Assets/EZFloatingWindow/EZFloatingWindow.cs
@@ -0,0 +1,52 @@
+﻿/* Author:          #AUTHORNAME#
+ * CreateTime:      #CREATETIME#
+ * Orgnization:     #ORGNIZATION#
+ * Description:     
+ */
+using UnityEngine;
+
+namespace EZhex1991
+{
+    public static class EZFloatingWindow
+    {
+#if UNITY_ANDROID
+        private static AndroidJavaClass unityPlayer;
+        private static AndroidJavaObject unityActivity;
+
+        private static AndroidJavaClass floatingWindowService;
+        private static AndroidJavaObject serviceIntent;
+#endif
+
+        public static void Init(string windowText)
+        {
+#if UNITY_ANDROID
+            unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            floatingWindowService = new AndroidJavaClass("com.ezhex1991.ezfloatingwindow.FloatingWindowService");
+            serviceIntent = floatingWindowService.CallStatic<AndroidJavaObject>("getServiceIntent", unityActivity, windowText);
+#endif
+        }
+
+        public static void GetPermission()
+        {
+#if UNITY_ANDROID
+            floatingWindowService.CallStatic("getPermission", unityActivity);
+#endif
+        }
+
+        public static void StartService()
+        {
+#if UNITY_ANDROID
+            floatingWindowService.CallStatic("startService", unityActivity, serviceIntent);
+#endif
+        }
+
+        public static void StopService()
+        {
+#if UNITY_ANDROID
+            floatingWindowService.CallStatic("stopService", unityActivity, serviceIntent);
+#endif
+        }
+    }
+}
---
+++ Assets/EZFloatingWindow/FloatingWindowTest.cs
@@ -0,0 +1,40 @@
+﻿/* Author:          #AUTHORNAME#
+ * CreateTime:      #CREATETIME#
+ * Orgnization:     #ORGNIZATION#
+ * Description:     
+ */
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EZhex1991
+{
+    public class FloatingWindowTest : MonoBehaviour
+    {
+        public Button button_GetPermission;
+        public Button button_StartService;
+        public Button button_StopService;
+
+        private void Start()
+        {
+            EZFloatingWindow.Init(Application.identifier);
+            button_GetPermission.onClick.AddListener(GetPermission);
+            button_StartService.onClick.AddListener(StartService);
+            button_StopService.onClick.AddListener(StopService);
+        }
+
+        private void GetPermission()
+        {
+            EZFloatingWindow.GetPermission();
+        }
+
+        private void StartService()
+        {
+            EZFloatingWindow.StartService();
+        }
+
+        private void StopService()
+        {
+            EZFloatingWindow.StopService();
+        }
+    }
+}
